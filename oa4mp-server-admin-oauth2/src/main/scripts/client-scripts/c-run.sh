086acdf0dcbfd683cfb094ea8ffcfb6f64bfba72
Jeff Gaynor <gaynor@illinois.edu>
2017-04-11 13:41:53 -0500

    Client regsitration management example script

--- oa4mp-server-admin-oauth2/src/main/scripts/client-scripts/c-run.sh
+++ oa4mp-server-admin-oauth2/src/main/scripts/client-scripts/c-run.sh
@@ -1,5 +1,5 @@
 # DEFAULT_SERVER=https://locahost/oauth2/clients
-SERVER=http://localhost:44444/oauth2/clients
+# SERVER=http://localhost:44444/oauth2/clients
 sed -e "s|CLIENT_ID|$CLIENT_ID|" -e "s|CLIENT_SECRET|$CLIENT_SECRET|" -e "s|ADMIN_ID|$ADMIN_ID|" -e "s|ADMIN_SECRET|$ADMIN_SECRET|" $1 > input.json
 curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data @input.json $SERVER > output.json
 cat output.json
