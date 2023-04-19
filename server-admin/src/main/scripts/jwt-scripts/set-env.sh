ba25384a85b77d986d76318eb5ff765dcb98b20c
gaynor@illinois.edu <vnlH814i>
2023-04-19 15:59:21 -0500

    Weirdly, a bunch of files seem to have removed themselves from GIT. Recommitting.

---
+++ server-admin/src/main/scripts/jwt-scripts/set-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This file contains the environment variables for the service. Set them here and they should
+# get picked up by each script as needed (this assumes everything is being run from the current
+# directory).
+
+
+
+#export JWT_JAR=jwt.jar
+#export JWT_JAR=/home/ncsa/dev/ncsa-git/oa4mp/oa4mp-server-admin-oauth2/target/jwt-jar-with-dependencies.jar
+export JWT_JAR=/home/ncsa/dev/temp-deploy/server/jwt.jar
+export JWT_LOG=/tmp/jwt.log
