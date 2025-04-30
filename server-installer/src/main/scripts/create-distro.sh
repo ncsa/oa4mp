cf5b9f9c47c0dcc40edbd41f98423c354f8a5646
jjg-123 <gaynor@illinois.edu>
2025-04-30 08:41:55 -0500

    6.1 RC0

--- server-installer/src/main/scripts/create-distro.sh
+++ server-installer/src/main/scripts/create-distro.sh
@@ -33,7 +33,7 @@ echo "  creating target dirs for installer in $PWD"
 # **IF** there are arguments for the target of this, use them. Otherwise use the default
 OA4MP_ROOT=${1:-$DEFAULT_OA4MP_ROOT}
 
-echo "OA4MP Version: 6.x-SNAPSHOT" > version.txt
+echo "OA4MP Version: 6.1-rc0" > version.txt
 
 # Now make the directories
 mkdir "bin"
